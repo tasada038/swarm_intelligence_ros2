2ecd4a5a4db99424c67e45717963a8846a4f9254
tasada038 <tasada381@gmail.com>
2024-06-29 17:20:28 +0900

    revised LICENSE

--- include/swarm_intelligence_ros2/mrfo/mrfo_component.hpp
+++ include/swarm_intelligence_ros2/mrfo/mrfo_component.hpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2024 Takumi Asada
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef SWARM_INTELLIGENCE_ROS2__MRFO__MRFO_COMPONENT_HPP_
 #define SWARM_INTELLIGENCE_ROS2__MRFO__MRFO_COMPONENT_HPP_
 
@@ -29,7 +43,7 @@ class MrfoComponent : public rclcpp::Node
 		rclcpp::TimerBase::SharedPtr timer_;
 		rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
 		rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr subscriber_;
-        void timer_callback(const std_msgs::msg::Float32::SharedPtr msg_data);		
+        void timer_callback(const std_msgs::msg::Float32::SharedPtr msg_data);
 		void manta_init();
 		void evaluate(int population);
 
